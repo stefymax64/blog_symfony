1ce48b65613f28bc77e0bce2ad6ff4283e7c5b19
Clémentine Cellier <stephanie.sccellier@gmail.com>
2023-02-08 12:10:01 +0100

    Symfony sécurité admin et user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
